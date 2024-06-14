4116bf37b4caedc762ef104facf6e840da440dfa
rafirafi03 <ahamedrafirafi03@gmail.com>
2024-06-14 18:54:08 +0530

    added login,register,profile

--- web-app/frontend/src/components/Hero.jsx
+++ web-app/frontend/src/components/Hero.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Container, Card, Button } from 'react-bootstrap';
+import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Hero = () => {
   return (
-    <div className='py-5'>
-        <Container className='d-flex justify-content-center'>
-            <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
-                <h1 className='text-center mb-4'>Mern Authentication</h1>
-                <p className='text-center mb-4'>
-                    This is a 
-                </p>
-                <div className='d-flex'>
-                    <Button variant='primary' href='/login' className='me-3'>
-                        Sign In
-                    </Button>
-                    <Button variant='secondary' href='/register'>
-                        Sign Up
-                    </Button>
-                </div>
-            </Card>
-        </Container>
-      
+    <div className="py-5">
+      <Container className="d-flex justify-content-center">
+        <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
+          <h1 className="text-center mb-4">Mern Authentication</h1>
+          <p className="text-center mb-4">This is a</p>
+          <div className="d-flex">
+            <LinkContainer to="/login">
+              <Button variant="primary" className="me-3">
+                Sign In
+              </Button>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Button variant="secondary">Sign Up</Button>
+            </LinkContainer>
+          </div>
+        </Card>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- web-app/frontend/src/main.jsx
+++ web-app/frontend/src/main.jsx
@@ -6,21 +6,34 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store";
+import { Provider} from "react-redux";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index={true} path="/" element={<HomeScreen/>}/>
-  </Route>
-)
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute/>}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
